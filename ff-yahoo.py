5883a149bd3d197fe7fe31fed01a7c6776aba142
Christian Weber <cmathew.weber@gmail.com>
2014-06-21 13:25:29 -0500

    barebones python script

---
+++ ff-yahoo.py
@@ -0,0 +1,18 @@
+import requests
+import rauth
+
+def login:
+	#Login information
+	oauth_consumer_key = ""
+	oauth_nonce = ""
+	oauth_signature_metho = ""
+	oauth_timestamp = ""
+	oauth_version = ""
+	xoauth_lang_pref = ""
+	oauth_callback = "oob:wq"
+
+if __name__ = "__main__":
+	login()
+else:
+	print "Please run as application, not module."
+	exit()
